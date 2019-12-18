ace4d4e84c9a4f808e5cd96c45dfbc99b65797e0
jonas_rocha <jonas_rocha@sicredi.com.br>
2019-12-18 09:58:17 -0300

    # Correção do novo endpoint da aplicação :)

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = flask.Flask(__name__)
 @app.route("/")
 def helloworld():
     return "Bem vindo ao Rebatedor de APIs! </br>" \
-           "Temos 01 API mapeada até o momento :D </br>" \
+           "Temos 02 APIs mapeadas até o momento :D </br>" \
            "GLHF!"
 
 
@@ -23,12 +23,12 @@ def acquirer_status(cpfcnpj):
 
 
 @app.route("/establishment/searchestablishment/<cpfcnpj>")
-def acquirer_status(cpfcnpj):
+def establishment(cpfcnpj):
     """
     Versão 1.0 da API de statuso do estabelecimento
     Retorna a massa de dados atribuída para o CNPJ/CPF escolhido
     """
-    return reader.file_open("massa/acquirer_status/", cpfcnpj+".txt")
+    return reader.file_open("massa/establishment/", cpfcnpj+".txt")
 
 
 app.run()
