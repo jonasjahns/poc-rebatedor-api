740dbbfea3ecba9b409bcd7bc67e1b06089c78e4
jonas_rocha <jonas_rocha@sicredi.com.br>
2019-12-24 09:36:05 -0300

    # Inclusão do rebatedor para:     - MDR_FEES     - SALES     - TERMINAL

--- app.py
+++ app.py
@@ -25,10 +25,38 @@ def acquirer_status(cpfcnpj):
 @app.route("/establishment/searchestablishment/<cpfcnpj>")
 def establishment(cpfcnpj):
     """
-    Versão 1.0 da API de statuso do estabelecimento
+    Versão 1.0 da API de status do estabelecimento
     Retorna a massa de dados atribuída para o CNPJ/CPF escolhido
     """
     return reader.file_open("massa/establishment/", cpfcnpj+".txt")
+
+
+@app.route("/sale/consultsale/realizadas/<merchant>/<datainicio>/<datafim>")
+def sales(merchant, datainicio, datafim):
+    """
+        Versão 1.0 da API de vendas
+        Retorna a massa de dados atribuída para o merchant escolhido
+        Não realiza filtro por mês
+    """
+    return reader.file_open("massa/sales/", merchant+".txt")
+
+
+@app.route("/terminal/consultterminal/<merchant>")
+def terminal(merchant):
+    """
+        Versão 1.0 da API de vendas
+        Retorna a massa de dados atribuída para o merchant escolhido
+    """
+    return reader.file_open("massa/terminal/", merchant+".txt")
+
+
+@app.route("/mdr-fees/getmdrfees/<merchant>")
+def mdr_fees(merchant):
+    """
+        Versão 1.0 da API de vendas
+        Retorna a massa de dados atribuída para o merchant escolhido
+    """
+    return reader.file_open("massa/mdr_fees/", merchant+".txt")
 
 
 app.run()
