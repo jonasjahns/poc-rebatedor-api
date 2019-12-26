8c0b5cb6ac1a4c6dc64af7775c4a48bc8aa92dc0
jonas_rocha <jonas_rocha@sicredi.com.br>
2019-12-26 13:47:46 -0300

    # Adicionado comentários

--- batch/api_lote.py
+++ batch/api_lote.py
@@ -13,25 +13,32 @@ import pandas as pd
 
 def retorna_agencia(cnpj):
     try:
+        # Chama a requisição da API
         r = requests.get("https://adquirencia-processadora-api.prd.rs-1.paas.sicredi.net" +
                          "/establishment/searchestablishment/00000004/" + cnpj,
                          verify=False, timeout=2)
     except:
+        # Retorna - se timeout
         return "-"
     try:
         data = json.loads(r.content)
     except:
-        print(r)
-        return "-"
+        # Retorna se problema de parse no json outros erros
+        return "+"
     return data['estabelecimentos'][0]['domiciliosBancario'][0]['numAgencia']
 
 
+# Lê o arquivo input.xlsx e carrega em um dataframe
 df = pd.read_excel("input2.xlsx", "1")
+# Abre o arquivo de saída
 filename = "saida.csv"
 myfile = open(filename, "w")
 
+# Para cada CNPJ lido no excel, busca a agência na api da Fiserv
 for cnpj in df.iterrows():
     agencia = retorna_agencia(str(cnpj[1]['cnpj']))
+    # Printa a agência encontrada e a quantidade de chamadas até o momento
     print("AGENCIA: ------" + agencia + "------")
     print(str(cnpj[0]) + "de 5032")
+    # Salva a agência, se encontrada, ou "-" se timeout
     myfile.write(str(cnpj[1]['cnpj']) + ";" + agencia + "\n")
